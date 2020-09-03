5940a9a78799f8590bca1cde0924c89d48ec9dd3
Silvana <skalliny@my.bcit.ca>
2020-09-02 23:05:17 -0700

    added build

---
+++ build/precache-manifest.8012ebd977f6d9ebdc74423ab60303c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d18a393bb9c3af3c372a8e98f1e5ff8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f1971722669b4a4b4e1",
+    "url": "/static/css/main.53630c35.chunk.css"
+  },
+  {
+    "revision": "0c5988b0f645c58e32b8",
+    "url": "/static/js/2.c04f4fd4.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/static/js/2.c04f4fd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f1971722669b4a4b4e1",
+    "url": "/static/js/main.17d43781.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
